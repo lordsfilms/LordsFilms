167e7b97d7a96b88e32e7ca7c0076ca7cf2a46a2
Lords Films <71879471+lordsfilms@users.noreply.github.com>
2023-04-02 00:46:32 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 ‘use strict’;
-importScripts(‘sw-toolbox.js’); toolbox.precache([“lordsfilms/LordsFilms/index.html”,]); toolbox.router.get(‘/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+importScripts(‘sw-toolbox.js’); toolbox.precache([“https://lordsfilms.github.io/LordsFilms/index.html”]); toolbox.router.get(‘/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
