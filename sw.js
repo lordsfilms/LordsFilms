c18de1a2e1a5cdf816c426968a80548781b8fdc1
Lords Films <71879471+lordsfilms@users.noreply.github.com>
2023-04-02 21:33:20 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 ‘use strict’;
-importScripts(‘sw-toolbox.js’); toolbox.precache([“glavnyy.html”,“music.html”,“Tvonline.html”,“serial.html”,“films.html”,“yumorikanaly.html”,“index.html”,”style/style.css”]); toolbox.router.get(‘/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+importScripts(‘sw-toolbox.js’); toolbox.precache([“music.html”,“Tvonline.html”,“serial.html”,“films.html”,“yumorikanaly.html”,“index.html”,”style/style.css”]); toolbox.router.get(‘/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
